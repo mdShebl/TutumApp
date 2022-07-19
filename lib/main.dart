8d66d49682f432649a9238f571c25597df66e18a
Mohamed Shebl <67174367+mdShebl@users.noreply.github.com>
2022-07-20 01:40:49 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-            home: InvCode(),
+            home: med(),
             //WelcomeScreen(),
       //
       //theme: ThemeData(fontFamily: 'Monteserrat'),
